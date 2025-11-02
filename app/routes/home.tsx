fe9a0f51e0cc1f2dbb3a51e76ed07b4b75e99464
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-11-02 13:22:22 +0100

    Refactor Home component and remove unused assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Inspector" },
+    {
+      name: "description",
+      content: "Get insights into your resume and land your dream job!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Performance</h1>
+          <h2>Get insights into your resume and land your dream job!</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
