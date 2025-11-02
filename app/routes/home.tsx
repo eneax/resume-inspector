0905eb4b1ef9deeec2ed9830e273ce4753bf14ba
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-11-02 14:02:42 +0100

    Add ResumeCard and ScoreCircle components and integrate them into Home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
 import { Navbar } from "~/components/Navbar";
+import { ResumeCard } from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,10 +19,18 @@ export default function Home() {
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Performance</h1>
           <h2>Get insights into your resume and land your dream job!</h2>
         </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
