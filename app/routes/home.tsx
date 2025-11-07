1341590d527b7b9716d4417a4a75e023cc8d7e33
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-11-07 21:40:08 +0100

    Refactor CSS and update SVG attributes for consistency

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -34,13 +34,13 @@ export default function Home() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="mx-auto size-20 text-gray-400"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
           ></path>
         </svg>
