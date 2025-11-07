75d535e0d63213c436680601313554c3c6850412
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-11-07 12:33:17 +0100

    Improve Home component layout and content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router";
 import type { Route } from "./+types/home";
 
 import { usePuterStore } from "~/lib/puter";
+
 import { Navbar } from "~/components/Navbar";
 import { ResumeCard } from "~/components/ResumeCard";
 
@@ -45,48 +46,60 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <main className="bg-main-pattern">
       <Navbar />
 
-      <section className="main-section">
-        <div className="page-heading py-16">
-          <h1>Track Your Applications & Resume Performance</h1>
+      <section className="lg:grid lg:h-screen lg:place-content-center">
+        <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-28">
+          <div className="mx-auto max-w-prose text-center">
+            <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
+              Instantly analyze, improve, and{" "}
+              <strong className="text-indigo-600"> perfect </strong> your resume
+            </h1>
 
-          {!loadingResumes && resumes?.length === 0 ? (
-            <h2>No resumes found. Upload a resume to get started.</h2>
-          ) : (
-            <h2>Get insights into your resume and land your dream job!</h2>
+            {!loadingResumes && resumes?.length === 0 ? (
+              <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+                No resumes found. Upload a resume to get started.
+              </p>
+            ) : (
+              <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+                Your personal career assistant, designed to help you stand out
+                in a competitive job market. Reviews your resume in seconds,
+                improve structure and keyword relevance. Get noticed by hiring
+                managers and ATS systems alike.
+              </p>
+            )}
+          </div>
+
+          {loadingResumes && (
+            <div className="flex flex-col items-center justify-center">
+              <img
+                src="/images/resume-scan-2.gif"
+                alt="resume scan"
+                className="w-[200px]"
+              />
+            </div>
+          )}
+
+          {!loadingResumes && resumes.length > 0 && (
+            <div className="resumes-section">
+              {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))}
+            </div>
+          )}
+
+          {!loadingResumes && resumes?.length === 0 && (
+            <div className="mt-4 flex justify-center gap-4 sm:mt-6">
+              <Link
+                to="/upload"
+                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+              >
+                Get Started
+              </Link>
+            </div>
           )}
         </div>
-
-        {loadingResumes && (
-          <div className="flex flex-col items-center justify-center">
-            <img
-              src="/images/resume-scan-2.gif"
-              alt="resume scan"
-              className="w-[200px]"
-            />
-          </div>
-        )}
-
-        {!loadingResumes && resumes.length > 0 && (
-          <div className="resumes-section">
-            {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
-            ))}
-          </div>
-        )}
-
-        {!loadingResumes && resumes?.length === 0 && (
-          <div className="flex flex-col items-center justify-center mt-10 gap-4">
-            <Link
-              to="/upload"
-              className="primary-button w-fit text-xl font-semibold"
-            >
-              Upload Resume
-            </Link>
-          </div>
-        )}
       </section>
     </main>
   );
