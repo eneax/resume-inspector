39f0405b5428231a548c036cf0fb70359e76a7b7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-11-07 10:25:16 +0100

    Add Reset component for deleting all app data

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
   route("/auth", "routes/auth.tsx"),
   route("/upload", "routes/upload.tsx"),
   route("/resume/:id", "routes/resume.tsx"),
+  route("/reset", "routes/reset.tsx"),
 ] satisfies RouteConfig;
